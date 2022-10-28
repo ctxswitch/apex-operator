697285506a80d8547ff423d8bd5be13ef6526e34
Rob Lyon <47508711+ctxswitch@users.noreply.github.com>
2022-10-27 20:12:33 -0700

    fix(statsd): convert statsd names (#20)
    Convert names to valid statsd characters.

--- pkg/output/statsd/statsd.go
+++ pkg/output/statsd/statsd.go
@@ -26,21 +26,22 @@ func New(cfg apexv1.StatsdOutput) (output.Output, error) {
 
 func (s *Statsd) Send(m []metric.Metric) {
 	for _, mm := range m {
+		name := convertName(mm.Name())
 		tags := makeTags(mm.Tags())
 		for _, v := range mm.Values() {
 			switch mm.Type() {
 			case metric.Counter:
-				_ = s.client.Count(mm.Name(), int64(v.(float64)), tags, 1)
+				_ = s.client.Count(name, int64(v.(float64)), tags, 1)
 			case metric.Gauge:
-				_ = s.client.Gauge(mm.Name(), v.(float64), tags, 1)
+				_ = s.client.Gauge(name, v.(float64), tags, 1)
 			case metric.Histogram:
-				_ = s.client.Histogram(mm.Name(), v.(float64), tags, 1)
+				_ = s.client.Histogram(name, v.(float64), tags, 1)
 			case metric.Summary:
-				_ = s.client.Distribution(mm.Name(), v.(float64), tags, 1)
+				_ = s.client.Distribution(name, v.(float64), tags, 1)
 			case metric.Unknown:
-				_ = s.client.Gauge(mm.Name(), v.(float64), tags, 1)
+				_ = s.client.Gauge(name, v.(float64), tags, 1)
 			case metric.Untyped:
-				_ = s.client.Gauge(mm.Name(), v.(float64), tags, 1)
+				_ = s.client.Gauge(name, v.(float64), tags, 1)
 			}
 		}
 	}
@@ -58,4 +59,25 @@ func makeTags(t map[string]string) []string {
 		tags = append(tags, n+":"+v)
 	}
 	return tags
+}
+
+func convertName(name string) string {
+	bytes := []byte(name)
+	for i, b := range bytes {
+		switch {
+		case b >= 'a' && b <= 'z':
+			fallthrough
+		case b >= 'A' && b <= 'Z':
+			fallthrough
+		case b >= '0' && b <= '9':
+			fallthrough
+		case b == '.' || b == '_' || b == '-':
+			continue
+		case b == '/':
+			bytes[i] = '-'
+		default:
+			bytes[i] = '_'
+		}
+	}
+	return string(bytes)
 }
