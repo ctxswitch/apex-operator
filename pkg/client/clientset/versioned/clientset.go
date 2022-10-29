6e041586cd9536bb6b4f0d36006d542c28c03e7b
Rob Lyon <47508711+ctxswitch@users.noreply.github.com>
2022-10-28 18:26:02 -0700

    chore: update copyright (#25)

--- pkg/client/clientset/versioned/clientset.go
+++ pkg/client/clientset/versioned/clientset.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Rob Lyon <rob@ctxswitch.com>
+ * Copyright 2022 Rob Lyon <rob@ctxswitch.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
